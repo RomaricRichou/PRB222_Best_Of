9236cbacc7249f7913355aae8674852fd7e7ed0c
sall mouhamed <mouhamadou.sall@ensta-paris.fr>
2022-03-24 11:51:30 +0100

    ajout bestof

--- fonctions.cpp
+++ fonctions.cpp
@@ -1,1 +1,17 @@
+#include <stdlib.h>
+#include <math.h>
 
+const double PI=4*atan(1);
+
+double LN()
+{
+    double u=rand()/(double) RAND_MAX;
+    double v=rand()/(double) RAND_MAX;
+    return sqrt(-2*log(u))*cos(2*PI*v);
+}
+
+int main()
+    {
+        double s=LN();
+        return s;
+    }
