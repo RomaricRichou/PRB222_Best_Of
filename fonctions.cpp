9266cb96c3b7437956695c9c0d5bf909dc1ccf7b
RomaricRichou <81634406+RomaricRichou@users.noreply.github.com>
2022-03-31 12:18:54 +0200

    Add files via upload

--- fonctions.cpp
+++ fonctions.cpp
@@ -4,53 +4,67 @@
 #include "fonctions.hpp"
 #include "matrice.hpp"
 #include <algorithm>
+#include <time.h>
+
 const double PI=4*atan(1);
 
 double LN()
 {
     double u=rand()/(double) RAND_MAX;
+    while (u==0){
+        u=rand()/(double) RAND_MAX;
+    }
     double v=rand()/(double) RAND_MAX;
     return sqrt(-2*log(u))*cos(2*PI*v);
 }
 
-double mean(vector<double> v)
+double mean(const vector<double>& v)
 {
-    double som=0.;
+    long double som=0.;
+    int s =v.size();
     for (int i = 0; i < v.size(); i++)
     {
-        som+=v[i];
+        som+=(long double)(v[i])/s;
     }
-    return som/v.size();
-    
+    return som;
+
+}
+
+double var(const vector<double>& v){
+    vector<double> v2(v.size());
+    for (int i = 0; i < v.size(); i++)
+    {
+        v2[i]=v[i]*v[i];
+    }
+    double varr=mean(v2)-mean(v)*mean(v);
+    return varr;
 }
 
 
 void bestof::Wt_estim()
 {
+  if (n!=3){cout<<"Erreur, bestof codé uniquement pour n=3 pour le moment!"<<endl;exit(-1);}
   W.resize(n);
   vector<double> gaussien(n,0.);
-  for (int i = 0; i < n; i++)
-  {
-      gaussien[i]=LN();
-}
-    Matrice_carree A(n,0.);
-    A(1,1)=1;
-    A(2,1)=rho;
-    A(3,1)=rho;
-    A(2,2)=sqrt(1-rho*rho);
-    A(3,2)=rho*sqrt((1-rho)/(1+rho));
-    A(2,2)=sqrt(1-2*rho*rho/(1+rho));
-    
+  for (int i = 0; i < n; i++){
+    gaussien[i]=LN();
+  }
+  Matrice_carree A(n,0.);
+  A(1,1)=1;
+  A(2,1)=rho;
+  A(3,1)=rho;
+  A(2,2)=sqrt(1-rho*rho);
+  A(3,2)=rho*sqrt((1-rho)/(1+rho));
+  A(2,2)=sqrt(1-2*rho*rho/(1+rho));
 
-    // On forme W 
 
-    for (int i = 0; i < n; i++)
-    {
-        for ( int j = 0; j < n; j++)
-        {
+    // On forme W
+
+    for (int i = 0; i < n; i++){
+        W[i]=0;
+        for ( int j = 0; j < n; j++){
             W[i]+=A(i+1,j+1)*gaussien[j];
         }
-        
     }
 
 }
@@ -62,27 +76,126 @@ void bestof::St_estim()
     {
         S[i]=S0[i]*exp(r*T-0.5*sigma[i]*sigma[i]*T+W[i]*sigma[i]);
     }
-    
+
+}
+
+void bestof::St_estim_opp()
+{
+    S.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        S[i]=S0[i]*exp(r*T-0.5*sigma[i]*sigma[i]*T-W[i]*sigma[i]);
+    }
+
 }
 
 
 
 
-    
-double bestof::forward_MC_class(int nb_sim)
+
+
+void bestof::forward_MC_minvar(int nb_sim,bool indic) //indic = true pour call, false pour put.
 {
     vector<double> MC(nb_sim,0.);
-    for (int i = 0; i < nb_sim; i++)
-    {
+    for (int i = 0; i < nb_sim; i++){
         Wt_estim();
         St_estim();
-        double price=*max_element(S.begin(),S.end());
-        MC[i]=exp(-r*T)*(price-K);
-        
+        if (indic==true){
+            double price=*max_element(S.begin(),S.end());
+            MC[i]=exp(-r*T)*(price-K)/2;
+            St_estim_opp();
+            price=*max_element(S.begin(),S.end());
+            MC[i]+=exp(-r*T)*(price-K)/2;
+        }
+        else{
+            double price=*max_element(S.begin(),S.end());
+            MC[i]=exp(-r*T)*(K-price)/2;
+            St_estim_opp();
+            price=*max_element(S.begin(),S.end());
+            MC[i]+=exp(-r*T)*(K-price)/2;
+        }
     }
-    return mean(MC);
+    P= mean(MC);
+    varr= var(MC);
+    IC[0]=P*(1-(sqrt(varr)*1.645/sqrt(double(n))));
+    IC[1]=P*(1+(sqrt(varr)*1.645/sqrt(double(n))));
 
 }
 
+void bestof::forward_MC_class(int nb_sim,bool indic) //indic = true pour call, false pour put.
+{
+    vector<double> MC(nb_sim,0.);
+    for (int i = 0; i < nb_sim; i++){
+        Wt_estim();
+        St_estim();
+        if (indic==true){
+            double price=*max_element(S.begin(),S.end());
+            MC[i]=exp(-r*T)*(price-K);
+        }
+        else{
+            double price=*max_element(S.begin(),S.end());
+            MC[i]=exp(-r*T)*(K-price);
+        }
+    }
+    P= mean(MC);
+    varr= var(MC);
+    IC[0]=P*(1-(sqrt(varr)*1.645/sqrt(double(n))));
+    IC[1]=P*(1+(sqrt(varr)*1.645/sqrt(double(n))));
 
+}
 
+double positiv(double x){
+  double res =0;
+  if (x>0){
+    res=x;
+  }
+  return(res);
+};
+
+void bestof::option(int nb_sim,bool indic) //indic = true pour call, false pour put.
+{
+    vector<double> MC(nb_sim,0.);
+    for (int i = 0; i < nb_sim; i++){
+        Wt_estim();
+        St_estim();
+        if (indic==true){
+            double price=*max_element(S.begin(),S.end());
+            MC[i]=exp(-r*T)*positiv(price-K)/2;
+            St_estim_opp();
+            price=*max_element(S.begin(),S.end());
+            MC[i]+=exp(-r*T)*positiv(price-K)/2;
+        }
+        else{
+            double price=*max_element(S.begin(),S.end());
+            MC[i]=exp(-r*T)*positiv(K-price)/2;
+            St_estim_opp();
+            price=*max_element(S.begin(),S.end());
+            MC[i]+=exp(-r*T)*positiv(K-price)/2;
+
+        }
+    }
+    P= mean(MC);
+    varr= var(MC);
+    IC[0]=P*(1-(sqrt(varr)*1.645/sqrt(double(n))));
+    IC[1]=P*(1+(sqrt(varr)*1.645/sqrt(double(n))));
+
+}
+
+vector<double> linspace(double a, double b, int c){
+    vector<double> line(c);
+    double delta =(b-a)/(double)(c-1);
+    for (int i=0; i<c; i++){
+            line[i]=a + (i*delta);
+    }
+    return line;
+}
+
+void w_vector(const vector<double>& v,string file_name){
+    ofstream file;
+    remove(file_name.c_str());
+    file.open(file_name);
+    for (int i=0;i<v.size();i++){
+        file<<v[i]<<";";
+    }
+    file.close();
+}
--- fonctions.hpp
+++ fonctions.hpp
@@ -2,6 +2,11 @@
 #define FONCTIONS_HPP
 
 #include <vector>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -18,24 +23,34 @@ public:
     vector<double> W;
     vector<double> S;
     double P;
+    double varr;
+    vector<double> IC=vector<double>(2);
 
     bestof():n(3),r(0.02),rho(0.3),T(1.5),K(1)
     {S0= vector<double>(3,1); sigma =vector<double>(3,0.3);};
     bestof(int N,double rr, double rhoo, double t, double k, double s, double sig):n(N),r(rr),rho(rhoo),T(t),K(k)
     {S0= vector<double>(n,s); sigma =vector<double>(n,sig);};
-    
+
 
     void Wt_estim();
     void St_estim();
+    void St_estim_opp();
 
-    double forward_MC_class(int nb_sim);
-    double put();
-    double call();
+    void forward_MC_class(int nb_sim,bool indic);
+    void forward_MC_minvar(int nb_sim,bool indic);
 
+    void option(int nb_sim,bool indic);
 };
 
 double LN();
-double mean(vector<double> v);
+double mean(const vector<double>& v);
+double var(const vector<double>& v);
+
+double positiv(double x);
+
+vector<double> linspace(double a, double b, int c);
+
+void w_vector(const vector<double>& v,string file_name);
 
 #endif // FONCTIONS_HPP
 
